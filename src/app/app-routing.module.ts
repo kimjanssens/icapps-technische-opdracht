7d4d9fe3fef9077920636af5ffc4694de3bcbf5a
Kim Janssens <kim.janssens@studiohyperdrive.be>
2020-11-23 21:36:04 +0100

    feat: create projects module and list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'projects',
+    pathMatch: 'full'
+  },
+  {
+    path: 'projects',
+    loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
