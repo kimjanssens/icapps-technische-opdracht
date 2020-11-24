31476cad94bae814e2d465f264155641cd957de7
Kim Janssens <kim.janssens@studiohyperdrive.be>
2020-11-24 22:39:57 +0100

    feat: add projects filter

--- src/app/project/components/project-list/project-list.component.ts
+++ src/app/project/components/project-list/project-list.component.ts
@@ -3,6 +3,7 @@ import { Subject } from 'rxjs';
 import { finalize, takeUntil } from 'rxjs/operators';
 
 import { ProjectService } from '../../project.service';
+import { IProject } from '../../project.types';
 
 @Component({
   selector: 'app-project-list',
@@ -11,13 +12,24 @@ import { ProjectService } from '../../project.service';
 })
 export class ProjectListComponent implements OnInit, OnDestroy {
   public pageTitle = 'Projects';
-  public projects = [];
+  public projects: IProject[];
+  public filteredProjects: IProject[];
   public loading: boolean;
   public destroy$: Subject<boolean> = new Subject<boolean>();
+  public filter = '';
 
   constructor(
     private projectService: ProjectService
   ) { }
+
+  get searchText(): string {
+    return this.filter;
+  }
+
+  set searchText(value: string) {
+    this.filter = value;
+    this.filteredProjects = this.searchText ? this.filterProjects(this.searchText) : this.projects;
+  }
 
   public ngOnInit(): void {
     this.loading = true;
@@ -29,11 +41,20 @@ export class ProjectListComponent implements OnInit, OnDestroy {
       )
       .subscribe((projects) => {
         this.projects = projects;
+        this.filteredProjects = projects;
       });
   }
 
   public ngOnDestroy(): void {
     this.destroy$.next(true);
     this.destroy$.unsubscribe();
+  }
+
+  private filterProjects(filter: string): IProject[] {
+    filter = filter.toLocaleLowerCase();
+
+    return this.projects.filter((project: IProject) =>
+      project.name.toLocaleLowerCase().indexOf(filter) !== -1
+    );
   }
 }
--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ProjectRoutingModule } from './project-routing.module';
 import { ProjectListComponent } from './components/project-list/project-list.component';
@@ -13,7 +14,8 @@ import { ProjectDetailComponent } from './components/project-detail/project-deta
   ],
   imports: [
     CommonModule,
-    ProjectRoutingModule
+    ProjectRoutingModule,
+    FormsModule
   ]
 })
 export class ProjectModule { }
